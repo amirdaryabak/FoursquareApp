81d71b50b97cb6dec526df27e3382e696dd0702d
Amir Daryabak <amir.daryabak@gmail.com>
2020-06-23 12:06:52 +0430

    Initial commit

---
+++ app/src/main/java/com/amirdaryabak/foursquareapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amirdaryabak.foursquareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
